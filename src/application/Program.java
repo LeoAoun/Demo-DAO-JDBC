39368716f0d513aaef699cea863169b5d83c71dc
LeoAoun <leonardoaoundev@gmail.com>
2023-06-19 15:32:38 -0300

    SellerDAOJDBC and DAOFactory classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(1, "Leo", "leonardoaoundev@gmail.com", new Date(), 300.0, obj);
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 		
 		System.out.println(seller);
 	
