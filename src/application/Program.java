e5f5bc831288f53c5ab0923087e11cd75c349b56
LeoAoun <leonardoaoundev@gmail.com>
2023-06-25 23:15:58 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,30 +9,36 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
-		
+
 		System.out.println("==== TEST 1: seller findById =====");
 		Seller seller = sellerDAO.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n==== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDAO.findByDepartment(department);
-		for(Seller obj : list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n==== TEST 3: seller findAll =====");
 		list = sellerDAO.findAll();
-		for(Seller obj : list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n==== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Leo", "leo@gmail.com", new Date(), 17000.0, department);
 		sellerDAO.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n==== TEST 5: seller update =====");
+		seller = sellerDAO.findById(1);
+		seller.setName("Martha Waine");
+		sellerDAO.update(seller);
+		System.out.println("Update completed");
 	}
 }
